1a184b592efabc14361b5b53345f613fe2ec3d48
Akash Krishna <krishna@Akashs-MacBook-Air.local>
2025-02-23 20:51:53 +0530

    Fixed BaseTest setup

--- src/main/java/org/bookcart/base/BaseTest.java
+++ src/main/java/org/bookcart/base/BaseTest.java
@@ -2,7 +2,6 @@ package org.bookcart.base;
 
 import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
-import io.qameta.allure.Step;
 import org.bookcart.util.ConfigManager;
 import org.bookcart.util.CredentialsManager;
 import org.bookcart.util.ScreenshotUtils;
@@ -13,40 +12,46 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 
 public class BaseTest {
+    // Get the environment from system property or default to "qa"
+    protected static final String environment = System.getProperty("env", "qa");
+    protected static String username;
+    protected static String password;
+    protected static String baseUrl;
     // Logger for all child classes
     protected final CustomLogger logger = LogManager.getLogger(this.getClass());
     protected WebDriver driver;
-    protected String username;
-    protected String password;
-    private String baseUrl;
+
+    @BeforeSuite
+    public void setupGlobalConfiguration() {
+        logger.info("Environment: {}", environment);
+
+        // Fetch credentials for the environment using CredentialsManager
+        username = CredentialsManager.getUsername(environment);
+        password = CredentialsManager.getPassword(environment);
+
+        if (username == null || password == null) {
+            logger.warn("Credentials not found for environment: {}", environment);
+            throw new RuntimeException("Credentials are not configured for environment: " + environment);
+        }
+    }
+
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize(); // Maximizes the browser window
 
-        // Get the environment from system property or default to "qa"
-        String environment = System.getProperty("env", "qa");
-        logger.info("Environment: {}", environment);
-
         // Fetch the corresponding URL from config.properties
         baseUrl = ConfigManager.getProperty(environment + ".url");
-        Allure.step("Opened "+baseUrl);
-        
+        Allure.step("Opened " + baseUrl);
+
         if (baseUrl == null || baseUrl.isEmpty()) {
             logger.error("Base URL is missing in config.properties for {}", environment);
             throw new RuntimeException("Base URL is not configured in config.properties.");
-        }
-
-        // Fetch credentials for the environment using CredentialsManager
-        username = CredentialsManager.getUsername(environment);
-        password = CredentialsManager.getPassword(environment);
-        if (username == null || password == null) {
-            logger.warn("Credentials not found for environment: {}", environment);
-            throw new RuntimeException("Credentials are not configured for environment: " + environment);
         }
 
         // Open the application URL
