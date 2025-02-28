5e0a6cd7abebcd735b5bde0e406b2e442e559743
Akash Krishna <krishna@Akashs-MacBook-Air.local>
2025-02-28 11:39:53 +0530

    Added UserRegistrationFlow

--- src/test/java/org/bookcart/userregistration/UserRegistrationTest.java
+++ src/test/java/org/bookcart/userregistration/UserRegistrationTest.java
@@ -5,39 +5,33 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.TmsLink;
 import org.bookcart.base.BaseTest;
+import org.bookcart.flows.UserRegistrationFlow;
 import org.bookcart.model.User;
-import org.bookcart.pages.LoginPage;
-import org.bookcart.pages.RegisterPage;
-import org.bookcart.pages.common.Header;
 import org.bookcart.providers.UserDataProviders;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.bookcart.util.WaitUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class UserRegistrationTest extends BaseTest {
-    @Test(dataProviderClass = UserDataProviders.class, dataProvider = "defaultRegistrationData")
+
+    @Test(dataProviderClass = UserDataProviders.class,
+            dataProvider = "defaultRegistrationData")
     @Description("Verify that new user is able to register to the website")
     @Severity(SeverityLevel.CRITICAL)
     @TmsLink("CEL-TC-45")
-    public void userRegistrationTest(User user){
-        // Arrange
-        Header header = new Header(driver);
-        LoginPage loginPage = new LoginPage(driver);
-        RegisterPage registerPage = new RegisterPage(driver);
 
+    public void successfulUserRegistrationTest(User user) {
+        // Arrange
+        UserRegistrationFlow userRegistrationFlow = new UserRegistrationFlow(driver);
+        WaitUtils waitUtils = new WaitUtils(driver);
 
         // Act
-        header.clickOnLoginButton();
-        loginPage.clickOnRegisterButton();
-        registerPage.enterNewUserDetails(user);
-        registerPage.clickOnRegisterButton();
+        userRegistrationFlow.navigateToRegisterPageAndEnterUserDetails(user);
 
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.titleIs("Login"));
-        Assert.assertEquals(driver.getTitle(), "Login");
+        String expectedTitle = "Login";
+        String actualTitle = waitUtils.waitForTitle(expectedTitle);
+        Assert.assertEquals(actualTitle, expectedTitle,
+                "Expected " + expectedTitle + " page after registration");
 
     }
 }
