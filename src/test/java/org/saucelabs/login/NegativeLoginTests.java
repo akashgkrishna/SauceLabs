317e582d22bce6110625adbe8457655b13d8b662
Akash Krishna <krishna@Akashs-MacBook-Air.local>
2025-09-22 14:42:36 +0530

    Removed unwanted comments

--- src/test/java/org/saucelabs/login/NegativeLoginTests.java
+++ src/test/java/org/saucelabs/login/NegativeLoginTests.java
@@ -3,17 +3,11 @@ package org.saucelabs.login;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import io.qameta.allure.TmsLink;
 import org.saucelabs.base.BaseTest;
-import org.saucelabs.flows.LoginFlow;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.saucelabs.pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class NegativeLoginTests extends BaseTest {
     @DataProvider(name = "invalidLoginCredentials")
